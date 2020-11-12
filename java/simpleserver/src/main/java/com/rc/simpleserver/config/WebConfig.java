9b28f01c7e063440c6104272a704a7664a8a35f1
Roberto Cantore <rcantore@gmail.com>
2020-11-12 18:05:57 -0300

    logging requests

---
+++ java/simpleserver/src/main/java/com/rc/simpleserver/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.rc.simpleserver.config;
+
+import com.rc.simpleserver.controllers.interceptors.LoggerInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoggerInterceptor());
+    }
+}
