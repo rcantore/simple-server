cfee23e702b3905ff3f50a0a43b9b70d1847da53
Roberto Cantore <rcantore@gmail.com>
2022-08-23 18:33:33 -0300

    nuevas funcionalidades y docker

--- java/simpleserver/src/main/java/com/rc/simpleserver/controllers/DispatcherController.java
+++ java/simpleserver/src/main/java/com/rc/simpleserver/controllers/DispatcherController.java
@@ -1,23 +1,31 @@
 package com.rc.simpleserver.controllers;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.rc.simpleserver.config.EndpointsConfig;
 import com.rc.simpleserver.engine.endpoint.IEndPointDispatcher;
 import com.rc.simpleserver.engine.endpoint.factory.DispatcherFactory;
+import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@AllArgsConstructor
 public class DispatcherController {
     private static final Logger logger = LoggerFactory.getLogger(DispatcherController.class);
 
-    @Autowired
-    DispatcherFactory dispatcherFactory;
+    private final EndpointsConfig endpointsConfig;
+
+    private final DispatcherFactory dispatcherFactory;
 
     @RequestMapping(value="/{endPoint}*/**", method= RequestMethod.GET)
     public @ResponseBody String dispatch(@PathVariable String endPoint) {
         logger.info("endpoint " + endPoint);
+
+        logger.info("enpointsConfiug " + endpointsConfig.getEndpoint());
+        logger.info("enpointsConfiug " + endpointsConfig.getEndpoint().size());
+
         IEndPointDispatcher endPointDispatcher = dispatcherFactory.getEndPointDispatcher(endPoint);
 
         return endPointDispatcher.dispatchGet();
@@ -25,10 +33,25 @@ public class DispatcherController {
 
     @RequestMapping(value = "/{endPoint}*/**",
             method = RequestMethod.POST,
-            consumes = {MediaType.APPLICATION_JSON_VALUE, "application/vnd.lockstate+json", "application/x-www-form-urlencoded"},
-            produces = {MediaType.APPLICATION_JSON_VALUE, "application/vnd.lockstate+json", "application/x-www-form-urlencoded"})
+            consumes = {MediaType.APPLICATION_JSON_VALUE, "application/vnd.lockstate+json", "application/x-www-form-urlencoded", MediaType.APPLICATION_XML_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE, "application/vnd.lockstate+json", "application/x-www-form-urlencoded", MediaType.APPLICATION_XML_VALUE})
     public @ResponseBody String dispatchPost(@PathVariable String endPoint) {
         logger.info("endpoint " + endPoint);
+
+        logger.info("enpointsConfiug " + endpointsConfig.getEndpoint());
+        logger.info("enpointsConfiug " + endpointsConfig.getEndpoint().size());
+
+        IEndPointDispatcher endPointDispatcher = dispatcherFactory.getEndPointDispatcher(endPoint);
+
+        return endPointDispatcher.dispatchPost();
+    }
+
+
+    @RequestMapping(value = "/{endPoint}*/**",
+            method = RequestMethod.PUT,
+            consumes = {MediaType.APPLICATION_JSON_VALUE, "application/vnd.lockstate+json", "application/x-www-form-urlencoded", MediaType.APPLICATION_XML_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE, "application/vnd.lockstate+json", "application/x-www-form-urlencoded", MediaType.APPLICATION_XML_VALUE})
+    public @ResponseBody String dispatchPut(@PathVariable String endPoint) {
         IEndPointDispatcher endPointDispatcher = dispatcherFactory.getEndPointDispatcher(endPoint);
 
         return endPointDispatcher.dispatchPost();
